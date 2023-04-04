8f46de143245889292f55efc0d7e91f2da4f8ca6
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-04-04 08:34:36 +0300

    updated program

--- Linked-List/reverse_1.cpp
+++ Linked-List/reverse_1.cpp
@@ -11,6 +11,8 @@ class List{
     Node *head;
   public:
     List(std::string str);
+    List(const List& other); // Copy constructor
+    ~List();
     void display_linked_list(Node *head);
     Node* get_head() { return head; }
 };
@@ -18,16 +20,28 @@ class List{
 class reversed_linked_list{
     private:
     Node *head;
-    public:
+  public:
     reversed_linked_list(List list);
+    reversed_linked_list(const reversed_linked_list& other); // Copy constructor
+    ~reversed_linked_list();
     void display_linked_list(Node *head);
     Node* get_head(){return head;}
 };
+
 void reversed_linked_list::display_linked_list(Node *head){
     Node*ptr= head;
     while (ptr!=nullptr){
         std::cout<<ptr->data;
         ptr=ptr->next;
+    }
+}
+
+reversed_linked_list::~reversed_linked_list() {
+    Node* ptr = head;
+    while (ptr != nullptr) {
+        Node* next = ptr->next;
+        delete ptr;
+        ptr = next;
     }
 }
 
@@ -42,9 +56,29 @@ reversed_linked_list::reversed_linked_list(List list){
         previous=current;
         current=forward;
     }
-    head=previous;   
+    head=previous;
 }
 
+reversed_linked_list::reversed_linked_list(const reversed_linked_list& other) {
+    Node* current = other.head;
+    Node* new_head = nullptr;
+    Node* new_tail = nullptr;
+    while (current != nullptr) {
+        Node* new_node = new Node;
+        new_node->data = current->data;
+        new_node->next = nullptr;
+        if (new_head == nullptr) {
+            new_head = new_node;
+            new_tail = new_node;
+        }
+        else {
+            new_tail->next = new_node;
+            new_tail = new_node;
+        }
+        current = current->next;
+    }
+    head = new_head;
+}
 
 void List::display_linked_list(Node *head){
     Node*ptr= head;
@@ -52,6 +86,36 @@ void List::display_linked_list(Node *head){
         std::cout<<ptr->data;
         ptr=ptr->next;
     }
+}
+
+List::~List() {
+    Node* ptr = head;
+    while (ptr != nullptr) {
+        Node* next = ptr->next;
+        delete ptr;
+        ptr = next;
+    }
+}
+
+List::List(const List& other) {
+    Node* current = other.head;
+    Node* new_head = nullptr;
+    Node* new_tail = nullptr;
+    while (current != nullptr) {
+        Node* new_node = new Node;
+        new_node->data = current->data;
+        new_node->next = nullptr;
+        if (new_head == nullptr) {
+            new_head = new_node;
+            new_tail = new_node;
+        }
+        else {
+            new_tail->next = new_node;
+            new_tail = new_node;
+        }
+        current = current->next;
+    }
+    head = new_head;
 }
 
 List::List(std::string str){
@@ -80,8 +144,8 @@ int main(){
     //Node* reversed_head = myList.reverse_linked_list(myList.get_head());
     reversed_linked_list reversedList {myList};
     reversedList.display_linked_list(reversedList.get_head());
+
+
+
     return 0;
 }
-
-
-//This program has a memory leak. Try to fix it.
