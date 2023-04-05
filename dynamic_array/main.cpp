832cca0c298e21c0bb9e8524e9ccb4f6a0eaabf4
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-04-05 17:19:30 +0300

    updated code

---
+++ dynamic_array/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by rituparn on 05.04.23.
+//
+
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
